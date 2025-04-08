506512aaafdf9439a54c48476decc341c7a47352
n0xgg04 <dulieu.vblc@gmail.com>
2025-04-08 18:03:50 +0700

    Change default port from 3000 to 3123 in main application bootstrap

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { BotGateway } from './bot/bot.gateway';
 
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3123);
 
   const botGateway = app.get(BotGateway);
   botGateway.initEvent();
