163aee72bf1474d998fb0a123f3977175b893beb
n0xgg04 <dulieu.vblc@gmail.com>
2025-06-19 00:02:48 +0700

    Comment out message logging functionality in FomuLogService and add a new line in MezonModule for improved client initialization.

--- src/v2/message_log/fomu.service.ts
+++ src/v2/message_log/fomu.service.ts
@@ -11,22 +11,22 @@ export class FomuLogService {
   @OnEvent(Events.ChannelMessage)
   async handleChannelMessage(message: ChannelMessage) {
     if (message.display_name === 'FUMO') return;
-    await this.prisma.message_logs.create({
-      data: {
-        message_id: message.message_id!,
-        sender_avatar: message.avatar!,
-        sender_name: message.display_name!,
-        sender_id: message.sender_id,
-        sender_username: message.username!,
-        content: JSON.stringify(message.content),
-        channel_id: message.channel_id,
-        clan_id: message.clan_id!,
-        clan_avatar: message.clan_avatar!,
-        clan_name: message.clan_nick!,
-        clan_username: message.clan_id!,
-        channel_label: message.channel_label,
-        display_name: message.display_name,
-      },
-    });
+    // await this.prisma.message_logs.create({
+    //   data: {
+    //     message_id: message.message_id!,
+    //     sender_avatar: message.avatar!,
+    //     sender_name: message.display_name!,
+    //     sender_id: message.sender_id,
+    //     sender_username: message.username!,
+    //     content: JSON.stringify(message.content),
+    //     channel_id: message.channel_id,
+    //     clan_id: message.clan_id!,
+    //     clan_avatar: message.clan_avatar!,
+    //     clan_name: message.clan_nick!,
+    //     clan_username: message.clan_id!,
+    //     channel_label: message.channel_label,
+    //     display_name: message.display_name,
+    //   },
+    // });
   }
 }
